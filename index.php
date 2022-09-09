3edb077f4aeadd35a4c53c2c71f264ea30b31f59
Alexandre Bulcao <ambulcao@gmail.com>
2022-09-09 14:41:00 +0100

    Files Slim
    Configuring files slim for the project

--- index.php
+++ index.php
@@ -2,21 +2,31 @@
 
 require_once("vendor/autoload.php");
 require_once("./conexao.php");
+require_once("./class/Page.php");
+//require_once("./vendor/slim");
 
+/*use \Slim\Slim;*/
 
+/*use \Ecommerce\class\Page.html;*/
 
-$app = new \Slim\Slim();
+$app = new \Slim\slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
 	$conexao = new PDO('mysql:host=localhost;dbname=db_ecommerce','root','');
     
-	$result = $conexao->query("SELECT * FROM tb_products")->fetchAll();
+	/*$result = $conexao->query("SELECT * FROM tb_products")->fetchAll();
 
   foreach ($result as $item) {
     echo $item['desproduct'] . ' - ' . $item['vlprice'] . '<br />';
   }
+
+});*/
+
+	$page = new Page();
+
+	$page->setTpl("index");
 
 });
 
